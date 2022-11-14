dcd84ff06f173a9ef544fea7a58515995d52581a
NathanCb <combnathan02@gmail.com>
2022-11-14 23:46:24 +0100

    Adding questions FAQ to utils/data.js for example

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,3 +1,37 @@
-export const textOne = "Grant will be used to build a dedicated web app to give traders a view of all dYdX asset funding rates across multiple timeframes. The page will be interactive to include deep diving across different assets and historical analysis of changing rates. It will also include API functionalities to allow developer access."
-export const textTwo = "The grant will be used to build a web application that integrates Tradingview strategies into a dYdX Trading account to execute trades directly through a strategy. Users will be able to tap into Tradingview resources and test out different ..."
-export const textThree = "The Grant will be used to write a full research report and code samples that will determine optimal trading strategies for rewards maximization. The paper will be marketed across several platforms and shared among trading circles to help readership."
+export const textOne =
+  "Grant will be used to build a dedicated web app to give traders a view of all dYdX asset funding rates across multiple timeframes. The page will be interactive to include deep diving across different assets and historical analysis of changing rates. It will also include API functionalities to allow developer access.";
+export const textTwo =
+  "The grant will be used to build a web application that integrates Tradingview strategies into a dYdX Trading account to execute trades directly through a strategy. Users will be able to tap into Tradingview resources and test out different ...";
+export const textThree =
+  "The Grant will be used to write a full research report and code samples that will determine optimal trading strategies for rewards maximization. The paper will be marketed across several platforms and shared among trading circles to help readership.";
+
+export const questionHome = [
+  {
+    question: "How do I apply?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nulla risus, consectetur ut bibendum non, gravida non libero.",
+  },
+  {
+    question: "Can I apply as a U.S. based person/company?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nulla risus, consectetur ut bibendum non, gravida non libero.",
+  },
+  {
+    question: "How will I be compensated?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nulla risus, consectetur ut bibendum non, gravida non libero.",
+  },
+  {
+    question: "What type of project will qualify for a Grant?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nulla risus, consectetur ut bibendum non, gravida non libero.",
+  },
+  {
+    question: "What is the application timeline? When can I expect to hear back?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nulla risus, consectetur ut bibendum non, gravida non libero.",
+  },
+  {
+    question: "How can I increase my chances of getting funded?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nulla risus, consectetur ut bibendum non, gravida non libero.",
+  },
+  {
+    question: "How often do you approve new Rounds of funding?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nulla risus, consectetur ut bibendum non, gravida non libero.",
+  },
+];
